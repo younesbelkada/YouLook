0317b874fb05ab3e474a48e7dd81c6688cef78b6
younesbelkada <younesbelkada@gmail.com>
2020-12-08 18:13:35 +0100

    fix hide

--- main-fast.py
+++ main-fast.py
@@ -504,7 +504,7 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("YouLook - Fast annotator", "YouLook - Fast annotator"))
         self.pushButton.setText(_translate("MainWindow", "Next Image"))
-        self.pushButton_hide.setText(_translate("MainWindow", "Original Image"))
+        self.pushButton_hide.setText(_translate("MainWindow", "Show Original Image"))
         self.pushButton_3.setText(_translate("MainWindow", "Predict"))
         self.pushButton_4.setText(_translate("MainWindow", "Previous Image"))
         #self.pushButton_5.setText(_translate("MainWindow", "Load Model"))
